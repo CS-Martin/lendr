57901fd6288a393625731326fe7701b40be5e3c3
Jiankarlo Belarmino <jiankarlo.belarmino@planbook.com>
2025-07-13 15:21:34 +0800

    feat: initialized nft resource

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from '@prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { NftModule } from './nft/nft.module';
 
 @Module({
-    imports: [PrismaModule, UsersModule],
+    imports: [PrismaModule, UsersModule, NftModule],
     controllers: [AppController],
     providers: [AppService],
 })
