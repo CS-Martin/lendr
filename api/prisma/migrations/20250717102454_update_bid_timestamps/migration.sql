a3404c2524e3df223d93cff7d8a8d0d029a6e768
Jiankarlo Belarmino <jiankarlo.belarmino@planbook.com>
2025-07-17 18:25:15 +0800

    feat: updated bid timestamps

--- api/prisma/migrations/20250717102454_update_bid_timestamps/migration.sql
+++ api/prisma/migrations/20250717102454_update_bid_timestamps/migration.sql
@@ -1,16 +1,15 @@
 /*
   Warnings:
 
-  - Added the required column `updatedAt` to the `Bid` table without a default value. This is not possible if the table is not empty.
+  - Made the column `updatedAt` on table `Bid` required. This step will fail if there are existing NULL values in that column.
   - Added the required column `updatedAt` to the `NFT` table without a default value. This is not possible if the table is not empty.
   - Added the required column `updatedAt` to the `Rental` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `updatedAt` to the `RentalPost` table without a default value. This is not possible if the table is not empty.
   - Added the required column `updatedAt` to the `RentalPostItem` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
-ALTER TABLE "Bid" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+ALTER TABLE "Bid" ALTER COLUMN "createdAt" SET DEFAULT CURRENT_TIMESTAMP,
+ALTER COLUMN "updatedAt" SET NOT NULL;
 
 -- AlterTable
 ALTER TABLE "NFT" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -18,10 +17,6 @@ ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
 
 -- AlterTable
 ALTER TABLE "Rental" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
-
--- AlterTable
-ALTER TABLE "RentalPost" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
 
 -- AlterTable
