a1acaefc2dfc21fb1c14f09a813ceae5f9f4c62d
Jiankarlo Belarmino <jiankarlo.belarmino@planbook.com>
2025-07-18 04:50:09 +0800

    feat: added class validator

--- api/src/transaction-hash/dto/transaction-hash.dto.ts
+++ api/src/transaction-hash/dto/transaction-hash.dto.ts
@@ -1,15 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNumber } from 'class-validator';
 
 export class TransactionHashDto {
   @ApiProperty({
     description: 'Blockchain transaction hash',
     example: '0xabcdef1234567890deadbeefabcdef1234567890',
   })
+  @IsString()
   transactionHash!: string;
 
   @ApiProperty({
     description: 'Rental ID associated with the transaction',
-    example: '1',
+    example: 1,
   })
+  @IsNumber()
   rentalId!: number;
 }
