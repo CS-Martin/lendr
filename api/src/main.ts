274e13a448bb8ec95e08dcc8b80c5b711559e6dc
Jiankarlo Belarmino <jiankarlo.belarmino@planbook.com>
2025-07-17 20:54:59 +0800

    feat: added api versioning and query validation

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,9 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.enableVersioning({
+        type: VersioningType.URI,
+        defaultVersion: '1',
+    });
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true, 
+        }),
+    );
 
     const config = new DocumentBuilder()
         .setTitle('Lendr API')
