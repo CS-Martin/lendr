ee8c93626f3cf881f565f3cbb6f6016db5e4ed83
martin <meatole.cs@gmail.com>
2025-07-28 11:35:14 +0800

    feat(client/nft): created nft api service

---
+++ apps/client/src/services/nft.api.ts
@@ -0,0 +1,73 @@
+import { config } from '@/config/env';
+import { logger } from '@/lib/logger';
+import { CreateNftDto, NftDto, ResponseDto } from '@repo/shared-dtos';
+
+export class NFTApiService {
+  private API_BASE_URL: string;
+
+  constructor() {
+    this.API_BASE_URL = `${config.api.BASE_URL}/${config.api.VERSION}/nft`;
+  }
+
+  public async create(createNftDto: CreateNftDto): Promise<ResponseDto<NftDto>> {
+    logger.info('Creating NFT:', createNftDto);
+
+    try {
+      const response = await fetch(`${this.API_BASE_URL}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(createNftDto),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error creating NFT:', error);
+      throw error;
+    }
+  }
+
+  public async findAll(): Promise<ResponseDto<NftDto[]>> {
+    logger.info('Fetching all NFTs');
+
+    try {
+      const response = await fetch(`${this.API_BASE_URL}`);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching NFTs:', error);
+
+      throw error;
+    }
+  }
+
+  public async findOne(address: string): Promise<ResponseDto<NftDto>> {
+    logger.info('Fetching NFT by address:', address);
+
+    try {
+      const response = await fetch(`${this.API_BASE_URL}/${address}`);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching NFT by address:', error);
+
+      throw error;
+    }
+  }
+}
