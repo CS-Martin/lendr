edb114553ac7f23a3214f6327d52ce6032f96c84
garlicdoggoe <garlicspoofiyyy@gmail.com>
2025-09-21 18:54:47 +0800

    add: sepolia network

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -15,14 +15,7 @@ export function getWagmiConfig(): ReturnType<typeof getDefaultConfig> {
   _config = getDefaultConfig({
     appName: 'Lendr',
     projectId,
-    chains: [
-      mainnet,
-      polygon,
-      optimism,
-      arbitrum,
-      base,
-      sepolia,
-    ],
+    chains: [mainnet, polygon, optimism, arbitrum, base, sepolia],
     ssr: true,
   });
 
